9a7d221a122a5c41d878a47ba9063be5657ee838
iskoti <iskoti@pm.me>
2021-06-12 13:06:44 +0200

    added links to github

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import Bucket from './components/Bucket';
 function App() {
   return (
     <React.Fragment>
-      <PageTitle>
+      <Text>
         Hi! This is an example of a very basic shop and shopping cart and
         passing items between the two.
-      </PageTitle>
+      </Text>
       <Wrapper>
         <Bucket bucket='shop' />
         <Bucket bucket='cart' />
       </Wrapper>
+      <Text>links to my other examples:</Text>
+      <Text>
+        <a href='https://gotta-do.github.io'>note-taking app</a>
+      </Text>
+      <FooterText>
+        Development by<a href='http://www.github.com/iskoti'> iskoti</a>
+      </FooterText>
     </React.Fragment>
   );
 }
@@ -25,8 +32,16 @@ const Wrapper = styled.div`
   justify-content: center;
   margin: 20px 50px;
 `;
-const PageTitle = styled.h1`
+const Text = styled.h1`
   color: #4f6d7a;
   text-align: center;
   margin: 50px 100px;
+  text-decoration: none;
+`;
+
+const FooterText = styled.h6`
+  color: #4f6d7a;
+  text-align: center;
+  margin: 10px 100px;
+  text-decoration: none;
 `;
